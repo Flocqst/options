07a03389c3376df2b0c10cdf094e0520c91f4cc1
Flocqst <flocastet@icloud.com>
2023-10-05 17:19:28 +0200

    option page grid layout

--- app/(dapp)/options/page.tsx
+++ app/(dapp)/options/page.tsx
@@ -4,12 +4,21 @@ import { SelectMarket } from "@/components/select-market";
 
 export default function Page() {
   return (
-    <section className="grid grid-cols-6 grid-rows-6 gap-4 px-4">
+    <section className="grid grid-cols-5 grid-rows-12 gap-4 px-4">
       <div className="">
         <SelectMarket />
       </div>
-      <div className="col-start-2 col-span-4 row-start-2 row-span-2">
+      <div className="bg-gray-700 col-start-2 col-span-4 row-start-1 row-span-1">
+        TODO : market info
+      </div>
+      <div className="col-start-2 col-span-4 row-start-2 row-span-4">
         <PriceChart />
+      </div>
+      <div className="bg-gray-700 col-start-1 row-start-2 row-span-4">
+        todo : strike dates
+      </div>
+      <div className="bg-gray-700	col-start-1 col-span-5 row-start-6 row-span-6">
+        TODO : Option trading interface
       </div>
     </section>
   )
